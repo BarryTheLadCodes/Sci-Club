c5948d76a284fff1f82a367a7ba90369bc35c96f
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-02-23 18:51:25 -0800

    finished blinking led code

--- blinking_leds_arduino/blinking_leds_arduino.ino
+++ blinking_leds_arduino/blinking_leds_arduino.ino
@@ -15,17 +15,13 @@ void loop() {
     digitalWrite(led1, HIGH);
     delay(1000);
     digitalWrite(led1, LOW);
-    delay(1000);
     digitalWrite(led2, HIGH);
     delay(1000);
     digitalWrite(led2, LOW);
-    delay(1000);
     digitalWrite(led3, HIGH);
     delay(1000);
     digitalWrite(led3, LOW);
-    delay(1000);
     digitalWrite(led4, HIGH);
     delay(1000);
     digitalWrite(led4, LOW);
-    delay(1000);
 }
