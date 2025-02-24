23832e39323254d5907bb2d93820aaceebbb7fa2
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-02-23 18:49:07 -0800

    first

---
+++ blinking_leds_arduino/blinking_leds_arduino.ino
@@ -0,0 +1,31 @@
+const int led1 = 9;
+const int led2 = 7;
+const int led3 = 5;
+const int led4 = 3;
+
+void setup() {
+    // set the digital pin as output:
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);  
+    pinMode(led3, OUTPUT);
+    pinMode(led4, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(led1, HIGH);
+    delay(1000);
+    digitalWrite(led1, LOW);
+    delay(1000);
+    digitalWrite(led2, HIGH);
+    delay(1000);
+    digitalWrite(led2, LOW);
+    delay(1000);
+    digitalWrite(led3, HIGH);
+    delay(1000);
+    digitalWrite(led3, LOW);
+    delay(1000);
+    digitalWrite(led4, HIGH);
+    delay(1000);
+    digitalWrite(led4, LOW);
+    delay(1000);
+}
